984433fcb0b838810787d8b1ac974e3a3d778ca7
Owuraa <emmanueloteng17@gmail.com>
2024-07-16 13:05:09 -0700

    Lesson2.py

---
+++ Lesson2.py
@@ -0,0 +1,20 @@
+#letters = ("a","b", "c", "d","e")
+
+#print(letters)
+
+
+#a= #nt(input("Enter the values for a: "))
+
+#b= #int(input("Enter the values for b: "))
+
+#result = a*b
+
+#print(result)
+
+a= float(input("Enter the value for a: "))
+
+b= float(input("Enter the value for b: "))
+
+result = a*b
+
+print(result)
